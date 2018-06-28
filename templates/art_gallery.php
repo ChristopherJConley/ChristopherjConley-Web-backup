8f58816c33e8f21e4df1ec547565175cabae6529
ChristopherJConley <34147816+ChristopherJConley@users.noreply.github.com>
2018-06-27 19:19:41 -0500

    Update 6/27/18
    Updating index.html for website

---
+++ templates/art_gallery.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link type="text/css" rel="stylesheet" href="/css/art_gallery.css">
+</head>
+<body>
+<!--banner section -->
+<div id="main">
+    <img src="/images/talihina1-banner.png" id="banner">
+    <div class="banner-text">
+      <h1>John Doe</h1>
+      <h5>John Doe</h5>
+      <h3>Photographer/ Nature Enthusiast</h3>
+    </div>
+</div>
+
+<!--Art Gallery Fully Responsive-->
+<div class="row">
+  <div class="column">
+    <img src="/images/talihina1.png" >
+    <img src="/images/talihina2.png" >
+    <img src="/images/talihina3.png" >
+    <img src="/images/talihina4.png" >
+  </div>
+  <div class="column">
+    <img src="/images/talihina5.png" >
+    <img src="/images/talihina6.png" >
+    <img src="/images/talihina7.png" >
+    <img src="/images/talihina8.png" >
+  </div>
+  <div class="column">
+    <img src="/images/talihina2.png" >
+    <img src="/images/talihina4.png" >
+    <img src="/images/talihina3.png" >
+    <img src="/images/talihina1.png" >
+  </div>
+  <div class="column">
+    <img src="/images/talihina7.png" >
+    <img src="/images/talihina6.png" >
+    <img src="/images/talihina5.png" >
+    <img src="/images/talihina8.png" >
+  </div>
+</div>
+<div id="about">
+  <p>Photographer bio here -- </p>
+
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nisi mi, ultrices nec lectus ullamcorper, dapibus dignissim justo. Curabitur pretium risus sed pretium egestas. Aliquam erat volutpat. Cras vitae ultricies leo, ac mattis libero. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris vulputate interdum venenatis. Nullam sit amet diam vel orci pulvinar tincidunt in in odio. Vivamus efficitur tellus commodo justo molestie volutpat. Sed vel sem eget turpis pellentesque sodales. Integer et ante volutpat, lobortis erat non, fermentum ligula. Nulla pretium massa id quam finibus malesuada. Etiam sagittis libero a velit sagittis, sit amet elementum nulla viverra. Nunc vestibulum felis odio. Pellentesque vel vehicula ligula.
+
+Cras vitae erat imperdiet, placerat libero eu, blandit ante. Phasellus elementum facilisis dui, accumsan dictum neque volutpat eu. Suspendisse laoreet dapibus turpis a imperdiet. Pellentesque consectetur eget purus ut porta. Nulla tempor id sapien accumsan dignissim. Nam mattis ac magna vel auctor. Nullam sed felis bibendum, faucibus ex vitae, tempor est. Curabitur imperdiet lorem eget elit malesuada tincidunt. Fusce ac pulvinar odio. Nam id justo elit. Suspendisse facilisis orci vel magna consequat, nec facilisis purus aliquet. Curabitur aliquet congue ligula vitae maximus.</p>
+</div>
+<!--javascript section-->
+
+</body>
+</html>
